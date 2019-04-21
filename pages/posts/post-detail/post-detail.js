d4ee77c42f30126db4c1c31e6ff1bcdd817c3677
AqingCyan <xuekaiqi@creatshare.com>
2019-04-21 21:43:59 +0800

    core:  add a successful interactive method

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -2,9 +2,7 @@
 const data = require('../../../data/posts-data.js')
 
 Page({
-  data: {
-    
-  },
+  data: {},
   /**
    * 生命周期函数--监听页面加载
    */
@@ -40,13 +38,45 @@ Page({
     // 收藏变未收藏，未收藏变收藏
     postCollected = !postCollected;
     postsCollected[this.data.currentPostId] = postCollected
-    // 更新文章是否收藏的缓存值
-    wx.setStorageSync('posts_collected', postsCollected)
-    // 更新数据绑定遍历，从而实现切换图片
-    this.setData({
-      collected: postCollected
+    // 交互
+    // this.showModal(postsCollected, postCollected)
+    this.showToast(postsCollected, postCollected)
+  },
+
+  showToast: function(postsCollected, postCollected) {
+    wx.showToast({
+      title: postCollected ? '收藏成功' : '取消收藏',
+      duration: 1000,
+      success: (res) => {
+        // 更新文章是否收藏的缓存值
+        wx.setStorageSync('posts_collected', postsCollected)
+        // 更新数据绑定遍历，从而实现切换图片
+        this.setData({
+          collected: postCollected
+        })
+      }
     })
   },
 
-  onShareTap: function(event) {}
+  showModal: function(postsCollected, postCollected) {
+    wx.showModal({
+      title: "收藏",
+      content: postCollected ? "收藏该文章？" : "取消收藏该文章？",
+      showCancel: "true",
+      cancelText: "取消",
+      cancelColor: "#333",
+      confirmText: "确认",
+      confirmColor: "#405f80",
+      success: (res) => {
+        if (res.confirm) {
+          // 更新文章是否收藏的缓存值
+          wx.setStorageSync('posts_collected', postsCollected)
+          // 更新数据绑定遍历，从而实现切换图片
+          this.setData({
+            collected: postCollected
+          })
+        }
+      }
+    })
+  }
 })
