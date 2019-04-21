bc2cadf7e118853bcc5ba1e9c3e7393b5b9a630d
AqingCyan <xuekaiqi@creatshare.com>
2019-04-21 18:02:18 +0800

    UI: create detail page's style

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -15,5 +15,13 @@ Page({
     this.setData({
       posts_content: postData.postList
     })
+  },
+
+  onPostTap: function(event) {
+    // 获取自定义属性postid，以操作对应数据
+    let postId = event.currentTarget.dataset.postid
+    wx.navigateTo({
+      url: './post-detail/post-detail',
+    })
   }
 })
