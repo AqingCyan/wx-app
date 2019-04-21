b449d831532b80f89c7d02178c9f6434f5ed405c
AqingCyan <xuekaiqi@creatshare.com>
2019-04-21 21:10:17 +0800

    core: add collect function

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,9 +1,9 @@
 // pages/posts/posts-detail/posts-detail.js
-const data  = require('../../../data/posts-data.js')
+const data = require('../../../data/posts-data.js')
 
 Page({
   data: {
-
+    
   },
   /**
    * 生命周期函数--监听页面加载
@@ -11,10 +11,42 @@ Page({
   onLoad: function(options) {
     // 接收传递过来的id
     let postId = options.id
+    this.data.currentPostId = postId
     let postsData = data.postList
     let postData = postsData[postId]
     this.setData({
       postData
+    }) 
+
+    // 获取缓存查询收藏状态
+    let postsCollected = wx.getStorageSync('posts_collected')
+    if (postsCollected) {
+      let postCollected = postsCollected[postId]
+      if (postCollected) {
+        this.setData({
+          collected: postCollected
+        })
+      }
+    } else {
+      let postsCollected = {}
+      postsCollected[postId] = false
+      wx.setStorageSync('posts_collected', postsCollected)
+    }
+  },
+
+  onColletionTap: function(event) {
+    let postsCollected = wx.getStorageSync('posts_collected')
+    let postCollected = postsCollected[this.data.currentPostId]
+    // 收藏变未收藏，未收藏变收藏
+    postCollected = !postCollected;
+    postsCollected[this.data.currentPostId] = postCollected
+    // 更新文章是否收藏的缓存值
+    wx.setStorageSync('posts_collected', postsCollected)
+    // 更新数据绑定遍历，从而实现切换图片
+    this.setData({
+      collected: postCollected
     })
-  }
+  },
+
+  onShareTap: function(event) {}
 })
